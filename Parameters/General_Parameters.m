4ea8934667638a1058727bf1451a20e2886d8f7a
nicoloponcia <nicolo.poncia@mail.polimi.it>
2021-06-12 14:46:32 +0200

    added Dx_Influence
    forse funziona

--- Parameters/General_Parameters.m
+++ Parameters/General_Parameters.m
@@ -3,7 +3,7 @@ Smi.general.gravity             = [0 0 -9.80665];
 Smi.general.jounce_mode         = 1;           % 1=ramp 2=sine 3=random
 Smi.general.simulation_time     = 1;
 
-Smi.general.ramp_slope          = 80;
+Smi.general.ramp_slope          = 50;
 
 Smi.general.sine_amplitude      = 40;
 Smi.general.sine_frequency      = 2*pi;
---
+++ analysis/dx_influence.m
@@ -0,0 +1,49 @@
+% Calculates the influence of steering angle and jounce on camber
+
+
+%% Intro
+
+quarter_FIX;
+
+model = 'Parametrica';
+
+load_system(model);
+
+save_system(model);
+
+Smi.general.jounce_mode     = 1;           %jounce ramp mode
+Smi.general.simulation_time = 1;
+Smi.general.ramp_slope      = 50;
+dt                          =  0.001;     %[s]
+
+
+% sweep parameters
+
+dx                  =   0:1:70;                                  %range of steering angle tested [deg]
+imposed_steering    =   -150:5:150;                              %range of steering angle tested [deg]
+
+n_sim               =   numel(imposed_steering)*numel(dx);                 %number of simulation
+
+
+
+%% simulate each set
+
+simIn(1:n_sim) = Simulink.SimulationInput(model);
+% simIn(:) = simIn(:).setModelParameter('SolverType', 'Fixed-step');
+% simIn(:) = simIn(:).setModelParameter('FixedStep' ,  num2str(dt));
+
+tic
+for ii = 1:numel(dx)
+    for jj = 1:numel(imposed_steering)
+          
+          kk=((ii-1)*numel(imposed_steering)+jj);
+            
+          Smi = move_dx(dx(ii));
+          simIn(kk)=simIn(kk).setVariable('Smi' , Smi);
+    end
+end
+toc
+
+tic
+out = parsim(simIn,'TransferBaseWorkspaceVariables','on');
+toc
